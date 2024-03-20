f21d5d377b2af4b6d878fc1ba32d64f941824228
quochung <maphquochung@gmail.com>
2024-03-20 16:14:59 +0700

    update

--- common/tf_idf.py
+++ common/tf_idf.py
@@ -1,16 +1,26 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from collections import defaultdict
 import numpy as np
+from tqdm import tqdm
 
 class TfidfEmbeddingVectorizer(object):
-    with open("./data/glove.6B.100d.txt", "r", encoding="utf-8") as lines:
-        w2v = {line.split()[0]: np.array([float(i) for i in line.split()[1:]])
-           for line in lines}
+ 
+    def __init__(self, w2v_type="glove"):
+        
+        if w2v_type == "glove":
+            self.dim = 300
+            with open("./data/glove.42B.300d.txt", "r", encoding="utf-8") as lines:
+                self.w2v = {line.split()[0]: np.array([float(i) for i in line.split()[-self.dim:]])
+                for line in tqdm(lines)}
+        else:
+            self.dim = 300
+            with open("./data/word2vec_vi_syllables_300dims.txt", "r", encoding="utf-8") as lines:
+                self.w2v = {line.split()[0]: np.array([float(i) for i in line.split()[-self.dim:]])
+                for line in tqdm(lines)}
 
-    def __init__(self, word2vec = w2v):
-        self.word2vec = word2vec
+        self.word2vec = self.w2v
         self.word2weight = None
-        self.dim = 100
+        
 
     def fit(self, X, y):
         tfidf = TfidfVectorizer(analyzer=lambda x: x)
@@ -21,14 +31,26 @@ class TfidfEmbeddingVectorizer(object):
         max_idf = max(tfidf.idf_)
         self.word2weight = defaultdict(
             lambda: max_idf,
-            [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
+            [(w, tfidf.idf_[i]) for w, i in tqdm(tfidf.vocabulary_.items())])
 
         return self
+    
+    # def check_coverage(self, text):
+    #     for word in text.split():
+
 
     def transform(self, X):
+        
         return np.array([
                 np.mean([self.word2vec[w] * self.word2weight[w]
                          for w in words if w in self.word2vec] or
                         [np.zeros(self.dim)], axis=0)
                 for words in X
             ])
+
+if __name__ == "__main__":
+    trans = TfidfEmbeddingVectorizer()
+
+    print(len(trans.word2vec))
+    trans.fit(['nguyen quang hung'], [1])
+    print(trans.transform(['nguyen quang hung']))
--- logistic_regression.py
+++ logistic_regression.py
@@ -1,12 +1,12 @@
 from sklearn import linear_model
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 import pandas as pd
 from common.text_utils import normalized
 from common.text_pattern import TITLE_PATTERN
 from common.tf_idf import TfidfEmbeddingVectorizer
 from tqdm import tqdm
-
+import dill
 
 class LogisticClassifier():
     def __init__(self) -> None:
@@ -32,25 +32,66 @@ class LogisticClassifier():
 
         #drop_duplicate_again
         self.df_train.drop_duplicates(inplace = True)
+
+    
+    def check_coverage(self, text):
+        length = len(text.split())
+        count = 0
+        for word in text.split():
+            self.character.add(word)
+            if word in self.w2v.keys():
+                count+=1
+                self.in_coverage.add(word)
+        return length, count
+
     
 
     def train(self):
         print("Training")
+        w2v = TfidfEmbeddingVectorizer(w2v_type="glove")
         self.lr_pipeline  = Pipeline([
-            ("word2vec_vectorizer", TfidfEmbeddingVectorizer()),
-            ("svm", linear_model.LogisticRegression())
+            ("word2vec_vectorizer", w2v),
+            ("logistic_regression", linear_model.LogisticRegression())
         ])
+        
+        self.w2v = w2v.w2v
+
+        total_length = 0
+        coverage = 0
+        self.character = set()
+        self.in_coverage = set()
+
+        for text in self.df_train['name'].values:
+            length, count = self.check_coverage(text)
+            total_length += length
+            coverage +=count
+
+        print("Coverage: ", coverage/total_length, ", num words: ", len(self.character),", Coverage word: ", len(self.in_coverage)/len(self.character))
+                                                 
         self.lr_pipeline.fit(self.df_train['name'],self.df_train['label'])
         print("Done Traning")
         
-    def predict(self):
-        predict = self.lr_pipeline.predict(self.df_eval['name'])
-        acc = accuracy_score(predict, self.df_eval['label'])
-        return predict,acc
+    def evaluate(self):
+        prediction = self.lr_pipeline.predict(self.df_eval['name'])
+        print("Accuracy",accuracy_score(prediction, self.df_eval['label']))
+        print("Recall",recall_score(prediction, self.df_eval['label']))
+        print("Precision",precision_score(prediction, self.df_eval['label']))
+        print("f1",f1_score(prediction, self.df_eval['label']))
+        
+        df_compare = self.df_eval
+        df_compare['predict'] = prediction
+        df_compare.to_csv('predict/lr.csv')
+
+
+    def save_model(self):
+        with open("model/lrc_model.pkl",'wb') as f:
+            dill.dump(self.lr_pipeline,f)
+
         
 
 if __name__ == '__main__':
     lr_clf = LogisticClassifier()
     lr_clf.read_data()
     lr_clf.train()
-   
+    lr_clf.evaluate()
+    lr_clf.save_model()
